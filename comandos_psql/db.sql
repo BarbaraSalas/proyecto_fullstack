0e144b2fc3fe0a108ff8d5dc976d5e4c75264fdf
barbara <barbarasalas.blog@gmail.com>
2022-09-19 14:58:56 -0300

    hito 1

---
+++ comandos_psql/db.sql
@@ -0,0 +1,10 @@
+create database materiales;
+\c materiales
+
+create table profesores (id serial primary key, nombre varchar(80) , email varchar(80), is_approved boolean);
+create table cursos (id serial primary key, nombre varchar(80));
+create table asignaturas (id serial primary key, nombre varchar(80));
+
+create table planificacion (id serial primary key, id_profesor int references profesores(id), id_curso int references cursos(id), id_asignatura int references asignaturas(id));
+create table evaluacion (id serial primary key, id_profesor int references profesores(id), id_curso int references cursos(id), id_asignatura int references asignaturas(id));
+
